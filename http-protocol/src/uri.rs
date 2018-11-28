a0b315e3789b1a6c9fc97f4dc4b2dc3dcce16fa3
Bernd Vogt <bernd.vogt@sourcepit.org>
2018-11-28 22:03:06 +0100

    protocol...

--- http-protocol/src/uri.rs
+++ http-protocol/src/uri.rs
@@ -2,9 +2,181 @@
 
 use common_failures::prelude::*;
 
+use std::fmt::Write;
 use std::io::BufRead;
 
-// pchar  = unreserved | escaped | ":" | "@" | "&" | "=" | "+" | "$" | ","
+// segment       = *pchar *( ";" param )
+#[derive(Debug, PartialEq)]
+struct Segment {
+    pchars: Vec<Char>,
+    params: Option<Vec<Param>>,
+}
+
+impl std::fmt::Display for Segment {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for c in &self.pchars {
+            fmt.write_str(c.to_string().as_str())?;
+        }
+        if let Some(params) = &self.params {
+            for p in params {
+                fmt.write_char(';')?;
+                fmt.write_str(p.to_string().as_str())?;
+            }
+        }
+        Ok(())
+    }
+}
+
+fn segment(r: &mut BufRead) -> Result<Option<Segment>> {
+    let mut s = match pchars(r)? {
+        Some(pchars) => Some(Segment {
+            pchars: pchars,
+            params: None,
+        }),
+        None => None,
+    };
+    if let Some(s) = &mut s {
+        let mut params: Vec<Param> = Vec::new();
+        loop {
+            if let Some(c) = next_char(r)? {
+                if c.is(b';') {
+                    match param(r)? {
+                        Some(p) => {
+                            consume_char(r, &c);
+                            params.push(p);
+                        }
+                        None => break,
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        if !params.is_empty() {
+            s.params = Some(params);
+        }
+    }
+    Ok(s)
+}
+
+#[derive(Debug, PartialEq)]
+struct Param {
+    pchars: Vec<Char>,
+}
+
+impl std::fmt::Display for Param {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for c in &self.pchars {
+            fmt.write_str(c.to_string().as_str())?;
+        }
+        Ok(())
+    }
+}
+
+fn param(r: &mut BufRead) -> Result<Option<Param>> {
+    let p = match pchars(r)? {
+        Some(pchars) => Some(Param { pchars }),
+        None => None,
+    };
+    Ok(p)
+}
+
+fn pchars(r: &mut BufRead) -> Result<Option<Vec<Char>>> {
+    let mut param: Vec<Char> = Vec::new();
+    loop {
+        if let Some(c) = next_char(r)? {
+            if is_pchar(&c) {
+                consume_char(r, &c);
+                param.push(c);
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+    match param.is_empty() {
+        true => Ok(None),
+        false => Ok(Some(param)),
+    }
+}
+
+fn is_pchar(c: &Char) -> bool {
+    match c {
+        Char::Escaped(_) => true,
+        Char::Normal(b) => {
+            is_unreserved(*b) || match b {
+                b':' => true,
+                b'@' => true,
+                b'&' => true,
+                b'=' => true,
+                b'+' => true,
+                b'$' => true,
+                b',' => true,
+                _ => false,
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Char {
+    Normal(u8),
+    Escaped((u8, u8, u8)),
+}
+
+impl Char {
+    fn is(&self, byte: u8) -> bool {
+        match self {
+            Char::Normal(b) => *b == byte,
+            _ => false,
+        }
+    }
+}
+
+impl std::fmt::Display for Char {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match &self {
+            Char::Normal(b) => fmt.write_char(*b as char)?,
+            Char::Escaped(bytes) => {
+                fmt.write_char(bytes.0 as char)?;
+                fmt.write_char(bytes.1 as char)?;
+                fmt.write_char(bytes.2 as char)?;
+            }
+        };
+        Ok(())
+    }
+}
+
+fn next_char(r: &mut BufRead) -> Result<Option<Char>> {
+    let mut c = Ok(None);
+    let buf = r.fill_buf()?;
+    if buf.len() > 0 {
+        let b: u8 = buf[0];
+        if b == b'%' {
+            if buf.len() < 3 {
+                c = Err(format_err!("Unexpected end of escape sequence."));
+            } else {
+                let bytes = (b, buf[1], buf[2]);
+                if is_escaped(bytes) {
+                    c = Ok(Some(Char::Escaped(bytes)));
+                } else {
+                    c = Err(format_err!("Invalid escape sequence."));
+                }
+            }
+        } else {
+            c = Ok(Some(Char::Normal(b)));
+        }
+    }
+    c
+}
+
+fn consume_char(r: &mut BufRead, c: &Char) {
+    match c {
+        Char::Normal(_) => r.consume(1),
+        Char::Escaped(_) => r.consume(3),
+    }
+}
 
 enum Uric {
     Simple(u8),
@@ -130,4 +302,43 @@ fn is_up_alpha(b: u8) -> bool {
 
 fn is_digit(b: u8) -> bool {
     b >= 48 && b <= 57
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::io::prelude::*;
+
+    use std::io::BufReader;
+
+    #[test]
+    fn test_param() {
+        let mut r = BufReader::new("".as_bytes());
+        let p = param(&mut r).unwrap();
+        assert_eq!(None, p);
+
+        let mut r = BufReader::new("f oo".as_bytes());
+        let p = param(&mut r).unwrap().unwrap();
+        assert_eq!(
+            Param {
+                pchars: vec![Char::Normal(b'f')]
+            },
+            p
+        );
+        let p = param(&mut r).unwrap();
+        assert_eq!(None, p);
+        let c = next_char(&mut r).unwrap().unwrap();
+        consume_char(&mut r, &c);
+        assert_eq!(Char::Normal(b' '), c);
+        let p = param(&mut r).unwrap().unwrap();
+        assert_eq!(
+            Param {
+                pchars: vec![Char::Normal(b'o'), Char::Normal(b'o')]
+            },
+            p
+        );
+        let p = param(&mut r).unwrap();
+        assert_eq!(None, p);
+    }
 }
